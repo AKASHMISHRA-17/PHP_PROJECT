84d5a8ccc09bc87fee2b188a358439beb4853d6e
Akash Mishra <72619086+AKASHMISHRA-17@users.noreply.github.com>
2024-07-19 13:53:27 +0530

    Create login.php

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+</head>
+<body>
+    <h1>Login Page</h1>
+    <form>
+        username:
+        <input type="text" id="udata">
+        <br>
+        <br>
+        Password:
+        <input type="text"id="pd">
+        <br>
+        <br>
+        <input type="submit"id="loginbtn"value="Login">
+        <button><a href="http://akash2.local/task-3/register.php" target="_blank">Register</a></button>
+</form>
+</body>
+</html>
